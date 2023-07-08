063f54269fb212015eb341869718eaceee3ea502
Michael-Jetson <18769939178@163.com>
2023-07-08 20:21:28 +0800

    7

--- pcdet/datasets/dataset.py
+++ pcdet/datasets/dataset.py
@@ -1,7 +1,9 @@
-from collections import defaultdict # 当字典中的key不存在但被查找时，返回默认值，而不是keyError
+from collections import defaultdict
+# 当字典中的key不存在但被查找时，返回默认值，而不是keyError
 from pathlib import Path
 
 import numpy as np
+import torch
 import torch.utils.data as torch_data
 
 from ..utils import common_utils
@@ -13,27 +15,33 @@ from .processor.point_feature_encoder import PointFeatureEncoder
 class DatasetTemplate(torch_data.Dataset):
     def __init__(self, dataset_cfg=None, class_names=None, training=True, root_path=None, logger=None):
         super().__init__()
-        self.dataset_cfg = dataset_cfg # 数据集配置文件
-        self.training = training # 训练模式
-        self.class_names = class_names # 类别
-        self.logger = logger # 日志
-        self.root_path = root_path if root_path is not None else Path(self.dataset_cfg.DATA_PATH) # 数据集根目录
+        self.dataset_cfg = dataset_cfg# 数据集配置文件
+        self.training = training# 训练模式
+        self.class_names = class_names# 类别
+        self.logger = logger# 日志
+        self.root_path = root_path if root_path is not None else Path(self.dataset_cfg.DATA_PATH)
+        self.logger = logger
         if self.dataset_cfg is None or class_names is None:
             return
 
-        self.point_cloud_range = np.array(self.dataset_cfg.POINT_CLOUD_RANGE, dtype=np.float32) # 点云范围
+        #点云范围
+        self.point_cloud_range = np.array(self.dataset_cfg.POINT_CLOUD_RANGE, dtype=np.float32)
+        
         # 创建点云特征编码器类
         self.point_feature_encoder = PointFeatureEncoder(
             self.dataset_cfg.POINT_FEATURE_ENCODING,
             point_cloud_range=self.point_cloud_range
         )
-        # 创建数据增强器类
+
+        #数据增强器类
         self.data_augmentor = DataAugmentor(
             self.root_path, self.dataset_cfg.DATA_AUGMENTOR, self.class_names, logger=self.logger
         ) if self.training else None
-        # 创建数据预处理器类
+
+        #数据预处理类
         self.data_processor = DataProcessor(
-            self.dataset_cfg.DATA_PROCESSOR, point_cloud_range=self.point_cloud_range, training=self.training
+            self.dataset_cfg.DATA_PROCESSOR, point_cloud_range=self.point_cloud_range,
+            training=self.training, num_point_features=self.point_feature_encoder.num_point_features
         )
 
         self.grid_size = self.data_processor.grid_size # 网格数量 = 点云范围 / 体素大小
@@ -45,26 +53,25 @@ class DatasetTemplate(torch_data.Dataset):
             self.depth_downsample_factor = self.data_processor.depth_downsample_factor
         else:
             self.depth_downsample_factor = None
-
-    
+            
     @property
     def mode(self):
-        """@property 可以让对象像访问属性一样区访问方法 self.mode"""
+        """@property 可以让对象像访问属性一样区访问方法 self.mode，查看工作模式是训练还是测试"""
         return 'train' if self.training else 'test'
 
     def __getstate__(self):
         """Return state values to be pickled
         获取对象的属性（__init__中定义的属性,可以使用self.__dict__获取），返回去掉'logger'的属性dict
         """
-        d = dict(self.__dict__) 
+        d = dict(self.__dict__)
         del d['logger']
         return d
 
-    def __setstate__(self, d):
-        self.__dict__.update(d) # 根据字典d更新类的属性值
+    def __setstate__(self, d): # 根据字典d更新类的属性值
+        self.__dict__.update(d)
 
-    @staticmethod
-    def generate_prediction_dicts(batch_dict, pred_dicts, class_names, output_path=None):
+    def generate_prediction_dicts(self, batch_dict, pred_dicts, class_names, output_path=None):
+        #主要目的是生成预测结果的字典，这个字典包含了预测的边界框、得分、标签等信息
         # @staticmethod不需要表示自身对象的self和自身类的cls参数，就和使用函数一样
         # @classmethod也不需要self参数，但第一个参数需要是表示自身类的cls参数
         """
@@ -82,6 +89,41 @@ class DatasetTemplate(torch_data.Dataset):
         Returns:
 
         """
+        
+        def get_template_prediction(num_samples):
+            #定义内部函数，用于生成一个模板预测字典，包括名称、得分、边界框和预测标签
+            box_dim = 9 if self.dataset_cfg.get('TRAIN_WITH_SPEED', False) else 7
+            ret_dict = {
+                'name': np.zeros(num_samples), 'score': np.zeros(num_samples),
+                'boxes_lidar': np.zeros([num_samples, box_dim]), 'pred_labels': np.zeros(num_samples)
+            }
+            return ret_dict
+
+        def generate_single_sample_dict(box_dict):
+            #定义内部函数，用于生成单个样本的预测字典
+            pred_scores = box_dict['pred_scores'].cpu().numpy()
+            pred_boxes = box_dict['pred_boxes'].cpu().numpy()
+            pred_labels = box_dict['pred_labels'].cpu().numpy()
+            pred_dict = get_template_prediction(pred_scores.shape[0])
+            if pred_scores.shape[0] == 0:
+                return pred_dict
+
+            pred_dict['name'] = np.array(class_names)[pred_labels - 1]
+            pred_dict['score'] = pred_scores
+            pred_dict['boxes_lidar'] = pred_boxes
+            pred_dict['pred_labels'] = pred_labels
+
+            return pred_dict
+
+        annos = []
+        for index, box_dict in enumerate(pred_dicts):
+            single_pred_dict = generate_single_sample_dict(box_dict)
+            single_pred_dict['frame_id'] = batch_dict['frame_id'][index]
+            if 'metadata' in batch_dict:
+                single_pred_dict['metadata'] = batch_dict['metadata'][index]
+            annos.append(single_pred_dict)
+
+        return annos
 
     def merge_all_iters_to_one_epoch(self, merge=True, epochs=None):
         """
@@ -94,7 +136,6 @@ class DatasetTemplate(torch_data.Dataset):
             self._merge_all_iters_to_one_epoch = False
 
     def __len__(self):
-        # 类似c++中的虚函数，子类如果继承必须重写
         raise NotImplementedError
 
     def __getitem__(self, index):
@@ -110,6 +151,30 @@ class DatasetTemplate(torch_data.Dataset):
 
         """
         raise NotImplementedError
+
+    def set_lidar_aug_matrix(self, data_dict):
+        """
+            Get lidar augment matrix (4 x 4), which are used to recover orig point coordinates.
+        """
+        lidar_aug_matrix = np.eye(4)
+        if 'flip_y' in data_dict.keys():
+            flip_x = data_dict['flip_x']
+            flip_y = data_dict['flip_y']
+            if flip_x:
+                lidar_aug_matrix[:3,:3] = np.array([[1, 0, 0], [0, -1, 0], [0, 0, 1]]) @ lidar_aug_matrix[:3,:3]
+            if flip_y:
+                lidar_aug_matrix[:3,:3] = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, 1]]) @ lidar_aug_matrix[:3,:3]
+        if 'noise_rot' in data_dict.keys():
+            noise_rot = data_dict['noise_rot']
+            lidar_aug_matrix[:3,:3] = common_utils.angle2matrix(torch.tensor(noise_rot)) @ lidar_aug_matrix[:3,:3]
+        if 'noise_scale' in data_dict.keys():
+            noise_scale = data_dict['noise_scale']
+            lidar_aug_matrix[:3,:3] *= noise_scale
+        if 'noise_translate' in data_dict.keys():
+            noise_translate = data_dict['noise_translate']
+            lidar_aug_matrix[:3,3:4] = noise_translate.T
+        data_dict['lidar_aug_matrix'] = lidar_aug_matrix
+        return data_dict
 
     def prepare_data(self, data_dict):
         """
@@ -141,12 +206,17 @@ class DatasetTemplate(torch_data.Dataset):
             gt_boxes_mask = np.array([n in self.class_names for n in data_dict['gt_names']], dtype=np.bool_)
             # 数据增强 传入字典参数，**data_dict是将data_dict里面的key-value对都拿出来
             # 下面在原数据的基础上增加gt_boxes_mask，构造新的字典传入data_augmentor的forward函数
+            if 'calib' in data_dict:
+                calib = data_dict['calib']
             data_dict = self.data_augmentor.forward(
                 data_dict={
                     **data_dict,
                     'gt_boxes_mask': gt_boxes_mask
                 }
             )
+            if 'calib' in data_dict:
+                data_dict['calib'] = calib
+        data_dict = self.set_lidar_aug_matrix(data_dict)
 
         # 筛选需要检测的gt_boxes
         if data_dict.get('gt_boxes', None) is not None:
@@ -170,7 +240,6 @@ class DatasetTemplate(torch_data.Dataset):
         # 使用点的哪些属性 比如x,y,z等
         if data_dict.get('points', None) is not None:
             data_dict = self.point_feature_encoder.forward(data_dict)
-
         # 对点云进行预处理，包括移除超出point_cloud_range的点、 打乱点的顺序以及将点云转换为voxel
         data_dict = self.data_processor.forward(
             data_dict=data_dict
@@ -184,8 +253,8 @@ class DatasetTemplate(torch_data.Dataset):
             """
             new_index = np.random.randint(self.__len__())
             return self.__getitem__(new_index)
-
-        data_dict.pop('gt_names', None) # pop() 方法删除字典给定键 key 及对应的值，返回值为被删除的值
+        # pop() 方法删除字典给定键 key 及对应的值，返回值为被删除的值
+        data_dict.pop('gt_names', None)
 
         return data_dict
 
@@ -195,6 +264,7 @@ class DatasetTemplate(torch_data.Dataset):
         由于训练集中不同的点云的gt框个数不同，需要重写collate_batch函数，
         将不同item的boxes和labels等key放入list，返回batch_size的数据
         """
+
         # defaultdict创建一个带有默认返回值的字典，当key不存在时，返回默认值，list默认返回一个空
         data_dict = defaultdict(list)
         # 把batch里面的每个sample按照key-value合并
@@ -203,6 +273,7 @@ class DatasetTemplate(torch_data.Dataset):
                 data_dict[key].append(val)
         batch_size = len(batch_list)
         ret = {}
+        batch_size_ratio = 1
 
         # 将合并后的key内的value进行拼接，先获取最大值，构造空矩阵，不足的部分补0
         # 因为pytorch要求输入数据维度一致
@@ -212,9 +283,14 @@ class DatasetTemplate(torch_data.Dataset):
                 # voxel_coords: optional (num_voxels, 3)
                 # voxel_num_points: optional (num_voxels)
                 if key in ['voxels', 'voxel_num_points']:
+                    if isinstance(val[0], list):
+                        batch_size_ratio = len(val[0])
+                        val = [i for item in val for i in item]
                     ret[key] = np.concatenate(val, axis=0)
                 elif key in ['points', 'voxel_coords']:
                     coors = []
+                    if isinstance(val[0], list):
+                        val =  [i for item in val for i in item]
                     for i, coor in enumerate(val):
                         # 在每个坐标前面加上序号
                         coor_pad = np.pad(coor, ((0, 0), (1, 0)), mode='constant', constant_values=i)
@@ -226,18 +302,32 @@ class DatasetTemplate(torch_data.Dataset):
                             constant_values=i 表示第一维填充i
                         """
                         coors.append(coor_pad)
-                    ret[key] = np.concatenate(coors, axis=0) # （B, N, 4)
+                    ret[key] = np.concatenate(coors, axis=0)# （B, N, 4)
                 elif key in ['gt_boxes']:
-                    max_gt = max([len(x) for x in val]) # 获取一个batch中所有帧中3D box最大的数量
-                    batch_gt_boxes3d = np.zeros((batch_size, max_gt, val[0].shape[-1]), dtype=np.float32) # 构造空的box3d矩阵（B, N, 7）
+                    max_gt = max([len(x) for x in val])# 获取一个batch中所有帧中3D box最大的数量
+                    batch_gt_boxes3d = np.zeros((batch_size, max_gt, val[0].shape[-1]), dtype=np.float32)# 构造空的box3d矩阵（B, N, 7）
                     for k in range(batch_size):
-                        batch_gt_boxes3d[k, :val[k].__len__(), :] = val[k] #val[k]表示一个batch中的第k帧
+                        batch_gt_boxes3d[k, :val[k].__len__(), :] = val[k]#val[k]表示一个batch中的第k帧
                     ret[key] = batch_gt_boxes3d
-                # gt_boxes2d同gt_boxes
-                elif key in ['gt_boxes2d']: 
+
+                elif key in ['roi_boxes']:
+                    max_gt = max([x.shape[1] for x in val])
+                    batch_gt_boxes3d = np.zeros((batch_size, val[0].shape[0], max_gt, val[0].shape[-1]), dtype=np.float32)
+                    for k in range(batch_size):
+                        batch_gt_boxes3d[k,:, :val[k].shape[1], :] = val[k]
+                    ret[key] = batch_gt_boxes3d
+
+                elif key in ['roi_scores', 'roi_labels']:
+                    max_gt = max([x.shape[1] for x in val])
+                    batch_gt_boxes3d = np.zeros((batch_size, val[0].shape[0], max_gt), dtype=np.float32)
+                    for k in range(batch_size):
+                        batch_gt_boxes3d[k,:, :val[k].shape[1]] = val[k]
+                    ret[key] = batch_gt_boxes3d
+
+                elif key in ['gt_boxes2d']:
                     max_boxes = 0
                     max_boxes = max([len(x) for x in val])
-                    batch_boxes2d = np.zeros((batch_size, max_boxes, val[0].shape[-1]), dtype=np.float32) # (B, N, 4)
+                    batch_boxes2d = np.zeros((batch_size, max_boxes, val[0].shape[-1]), dtype=np.float32)
                     for k in range(batch_size):
                         if val[k].size > 0:
                             batch_boxes2d[k, :val[k].__len__(), :] = val[k]
@@ -257,7 +347,7 @@ class DatasetTemplate(torch_data.Dataset):
                         pad_w = common_utils.get_pad_params(desired_size=max_w, cur_size=image.shape[1])
                         pad_width = (pad_h, pad_w)
                         # Pad with nan, to be replaced later in the pipeline.
-                        pad_value = np.nan
+                        pad_value = 0
 
                         if key == "images":
                             pad_width = (pad_h, pad_w, (0, 0))
@@ -270,12 +360,28 @@ class DatasetTemplate(torch_data.Dataset):
                                            constant_values=pad_value)
 
                         images.append(image_pad)
-                    ret[key] = np.stack(images, axis=0) # (B, H, W, C)
+                    ret[key] = np.stack(images, axis=0)
+                elif key in ['calib']:
+                    ret[key] = val
+                elif key in ["points_2d"]:
+                    max_len = max([len(_val) for _val in val])
+                    pad_value = 0
+                    points = []
+                    for _points in val:
+                        pad_width = ((0, max_len-len(_points)), (0,0))
+                        points_pad = np.pad(_points,
+                                pad_width=pad_width,
+                                mode='constant',
+                                constant_values=pad_value)
+                        points.append(points_pad)
+                    ret[key] = np.stack(points, axis=0)
+                elif key in ['camera_imgs']:
+                    ret[key] = torch.stack([torch.stack(imgs,dim=0) for imgs in val],dim=0)
                 else:
                     ret[key] = np.stack(val, axis=0)
             except:
                 print('Error in collate_batch: key=%s' % key)
                 raise TypeError
 
-        ret['batch_size'] = batch_size
+        ret['batch_size'] = batch_size * batch_size_ratio
         return ret
